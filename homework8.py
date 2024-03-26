d5c55547a5c7f76b13fbfc48465defd121d8ff21
gudwinhub <107505033+gudwinhub@users.noreply.github.com>
2024-03-26 17:28:16 +0300

    ДЗ циклы

--- homework8.py
+++ homework8.py
@@ -1,6 +1,6 @@
 car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 for i in range(len(car)):
-    print( 'Я езжу на автомабиле марки ',car[i])
+    print( 'Я езжу на автомабиле марки ', car[i])
 print('это фсе!')
 
 cars_count = 0
