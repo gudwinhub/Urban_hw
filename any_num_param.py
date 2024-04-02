cd3c240121cbe156fbda6e763f69ed9a9cba6a37
gudwinhub <107505033+gudwinhub@users.noreply.github.com>
2024-04-02 16:55:27 +0300

    ДЗ циклы

--- any_num_param.py
+++ any_num_param.py
@@ -15,17 +15,21 @@ def any_num_parm(a, b=2, *args, **kwargs):
 
 lst = 1, 2, 3, 4, 5
 
-# any_num_parm(11) # позиционные параметры
-# print('')
-# any_num_parm(111, 222)
-# print('')
-# any_num_parm(111, *lst)
-# print('')
-# any_num_parm(111, 22, *lst)
-# print('')
+any_num_parm(11) # задаем значение параметра a, параметр b= по умолчанию
+print('')
+any_num_parm(111, 222) # задаем а и b
+print('')
+any_num_parm(111, *lst) # задаем а, b = первому элементу из списка
+print('')
+any_num_parm(111, 22, *lst)
+print('')
 
 dict = {'d1':333, 'd2':True, 'd3':'строка'}
 
-any_num_parm(2, 2, [], dict) # позиционные параметры + упакованные + именованные
-# print('')
-any_num_parm(111, 222,[], **dict) # позиционные параметры + упакованные + именованные
+any_num_parm(2, dict) # b = по умолчанию, далее словарь попадет в args
+print('')
+any_num_parm(2, **dict) # b = по умолчанию, далее словарь попадет в kwargs
+print('')
+any_num_parm(2, 22,  dict) # задаем значения параметров a, b  словарь попадает в args
+print('')
+any_num_parm(2, 22, **dict) # словарьь попадает в kwargs
