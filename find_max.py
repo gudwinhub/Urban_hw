bda0cf407b29650a530bc8005f23e7e678d428db
gudwinhub <107505033+gudwinhub@users.noreply.github.com>
2024-04-03 17:25:05 +0300

    ДЗ циклы

--- find_max.py
+++ find_max.py
@@ -9,17 +9,17 @@ def find_max(*args):
         n -= 1
         max_num = args[n]
         num_tmp = args[n - 1]
-        print(max_num, num_tmp)
+        print(type(max_num), type(num_tmp), type(args), sep = ' ')
         # find_max(*args[:n])
 
     if max_num > num_tmp:
+        args = list(args)
         args[n-1] = max_num
-        find_max(*args[:n])
-
+        # find_max(args[:n])
+        print(*args)
 
     # max_num = num_tmp
     # find_max(args[:n-1])
     # return max_num
 
-
-print(*find_max(11, 47, 35, 25))
+print(find_max(11, 47, 35, 25, 58))
