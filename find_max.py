9148b6b7bd9badd5bae2d675a3930b0b4422bdbb
gudwinhub <107505033+gudwinhub@users.noreply.github.com>
2024-04-04 11:32:10 +0300

    ДЗ циклы

--- find_max.py
+++ find_max.py
@@ -1,25 +1,26 @@
 def find_max(*args):
+    num_tmp = 0
+    # global num_tmp
     n = len(args)
     if not args:
         return (print('вы забыли ввести числа'))
     elif n == 1:
-        # return(max_num = args[n-1])
-        return (args[0])
-    elif n > 1:
+        num_tmp = args[0]
+        return num_tmp
+    else:
         n -= 1
         max_num = args[n]
-        num_tmp = args[n - 1]
-        print(type(max_num), type(num_tmp), type(args), sep = ' ')
-        # find_max(*args[:n])
-
-    if max_num > num_tmp:
+        # num_tmp = args[n - 1]
+        # print(type(max_num), type(num_tmp), type(args), sep = ' ')
         args = list(args)
-        args[n-1] = max_num
-        # find_max(args[:n])
-        print(*args)
+        num_tmp = find_max(*args[:n])
+        if max_num < num_tmp:
+            max_num = num_tmp
+            # print(max_num)
+        return max_num
+# вызов с заданием аргументов в теле вызова
+print(find_max(11, 47, 35, 89, 58, 33,78,24,78))
 
-    # max_num = num_tmp
-    # find_max(args[:n-1])
-    # return max_num
-
-print(find_max(11, 47, 35, 25, 58))
+# вызов с заданием аргументов через список
+lst = [56, 23, 45, 78, 89, 56, 23, 45, 78, 92, 56, 23, 45, 78, 92]
+print(find_max(*lst))
