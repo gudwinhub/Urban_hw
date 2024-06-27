51c6cb334f839d328d8d71d8ed5803777692763f
gudwinhub <107505033+gudwinhub@users.noreply.github.com>
2024-06-27 20:56:59 +0300

    Add files via upload

--- Cafe v6.py
+++ Cafe v6.py
@@ -7,54 +7,64 @@ from time import sleep
 from queue import Queue
 
 global queue_visitor
+queue_visitor = Queue()
 
 class Cafe(Thread):
-
-    def __init__(self, amount_tables = 3, *args):  # table_free = True
+    
+    def __init__(self,  amount_tables = 3, *args):  # table_free = True
         super().__init__(*args)
         self.amount_tables = amount_tables
-        # self.queue_visitor = queue.Queue()  # queue.Queue(maxsize= )
         self.tables = Table(amount_tables)
         self.lock = threading.Lock()
-        # self.visitors = []
-        
+        # self.queue_visitor = Queue()  # queue.Queue(maxsize= )
+
     # def visitor_arrival(self, name_visitor):
     #     visitor = Visitor(name_visitor, self.queue_visitor)
     #     self.visitors.append(visitor)
 
     def visitor_served(self, visitor):
-        self.num_busy_table = self.Table.get_free_table()
+        self.num_busy_table = self.tables.get_free_table()
+        # self.num_busy_table = Table.get_free_table()
         if self.num_busy_table != 0:  # если есть свободные столики (хотя бы один)
-            with lock:
-                Table.set_param(num_table = self.num_busy_table, is_busy = True) # меняем статус столика на Занято
+            with self.lock:
+                self.tables.set_param(num_table = self.num_busy_table, is_busy = True) # меняем статус столика на Занято
                 print(f'{visitor} занял столик № {self.num_busy_table})')
                 time.sleep(random.randint(4, 7))  # время обслуживания
-                Table.set_param(num_table = self.num_busy_table, is_busy = False)
+                self.tables.set_param(num_table = self.num_busy_table, is_busy = False)
                 print(f'Посетитель {visitor} поел и пошел ...ь')
         else:
             print(f'К сожалению свободных столиков нет! совсем нет! ...посетитель {visitor} ждет...')
 
     def __run__(self):
         # queue_visitor.get(k)
-        while not queue_visitor.empy():
-            num_vistr = queue_visitor.get(k)
-            visitor_served(num_vistr)
+        visitors = Visitor()  # задаем размер потока посетителей (по умолчанию = 20
+        visitors_arrival_thread = threading.Thread(target=visitors.thr_visitor)
+        visitors_arrival_thread.start()
+
+        if queue_visitor.empy():
+            time.sleep(1)
+        else:
+            while not queue_visitor.empy():
+                visitors_arrival_thread.join()
+                with self.lock:
+                    num_vistr = queue_visitor.get()
+                    Cafe.visitor_served(num_vistr)
         
 
 # ------------------------------------------------------------------------------
 # Клас Table  - генерит список свободных столов
 class Table:
     # паттерн Singleton - что бы объект класса Table был всегда только в одном экземпляре
-    __instance = None
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-        return cls.__instance
+    # __instance = None
+    # def __new__(cls, *args, **kwargs):
+    #     if cls.__instance is None:
+    #         cls.__instance = super().__new__(cls)
+    #     return cls.__instance
     
-    def __init__(self, num_table):
-        self.num_table = num_table
+    def __init__(self, amount_table):
+        self.amount_table = amount_table
         self.is_busy = False        #  is_busy = False  - столик не занят
-        self.tables = {key: False for key in range(1, num_table+1)} # генерим свободные столики
+        self.tables = {key: False for key in range(1, amount_table+1)} # генерим свободные столики
         print(self.tables)
     def get_free_table(self):    # получить номер свободного столика
         # условие поиска: False in tables.values() - если стол свободен результат = True
@@ -84,22 +94,24 @@ class Visitor(Thread):  # класс Посетитель - гененрируе
 
     def thr_visitor(self):
         for k in range(1, self.amount_vistrs + 1):
-            try:
-                self.lock.acquire()
+            with self.lock:
                 print('поток посетителей  получил блокировку')
                 queue_visitor.put(k)
                 print(f'Посетитель {k} желает отобедать занимает очередь в кафе')
-            finally:
-                self.lock.release()
+            #     self.lock.acquire()
+            #     print('поток посетителей  получил блокировку')
+            #     queue_visitor.put(k)
+            #     print(f'Посетитель {k} желает отобедать занимает очередь в кафе')
+            # finally:
+            #     self.lock.release()
             time.sleep(random.randint(1, 2)) # интервал между посетителями 1-2 сек
 
-
+# tables = Table(5)
 cafe = Cafe(5) # задаем количество столиков в кафе (по умолчанию = 3)
-visitors = Visitor() # задаем размер оока посетителей (по умолчанию = 20
-
-visitors_arrival_thread = threading.Thread(target=visitors.thr_visitor)
-visitors_arrival_thread.start()
 cafe.start()
+cafe.join()
 
-visitors.join()
-cafe.join(timeout = 2)
+# visitors = Visitor() # задаем размер потока посетителей (по умолчанию = 20
+# visitors_arrival_thread = threading.Thread(target=visitors.thr_visitor)
+# visitors_arrival_thread.start()
+# visitors_arrival_thread.join()
